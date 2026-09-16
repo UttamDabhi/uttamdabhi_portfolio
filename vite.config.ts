df6d1de1834278d31fda49f8d27f82c3359259c1
UttamDabhi <umdabhi2004@gmail.com>
2026-09-16 10:58:15 +0400

    Fix Netlify production build

--- vite.config.ts
+++ vite.config.ts
@@ -28,9 +28,9 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+  outDir: path.resolve(__dirname, "dist"),
+  emptyOutDir: true,
+},
   server: {
     fs: {
       strict: true,
